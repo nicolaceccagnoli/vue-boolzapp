8884e64a4cc188d20d2dd20b12f849f493e5a55b
Nicola Ceccagnoli <nicola.ceccagnoli.94@gmail.com>
2024-01-11 12:17:06 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Estraggo la funzione createApp dall'oggetto Vue
+const { createApp } = Vue;
+
+// Creo l'istanza di Vue da montare in pagina
+createApp({
+    data() {
+        return {
+            message: 'Template HTML CSS Vue',
+        };
+    }
+  // Monto l'istanza di Vue in pagina
+}).mount('#app');
